7edcb32eca6897a07e5eca36ba048b6846758416
sabaasad737-ship-it <sabaasad737@gmail.com>
2026-06-11 23:28:01 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+marks = 91
+
+if marks >  40:
+    print("Pass")
+if marks > 70:
+    print("B grade")
+if marks > 80:
+     print("A grade")
+if marks > 90:
+    print("A+ Grade")
+if marks == 100:
+    print("Topper")
